c8422bd0c8ae49affe71c738c9c23553b701ca14
Matt <bmatt0835@gmail.com>
2024-11-04 21:35:21 +0000

    Header CSS adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,21 @@ import NavBar from "./NavBar";
 
 const Header = () => {
     return (
-        <section className="flex items-center justify-between h-16 bg-gray-600 pl-10">
-            <h1 className=" text-2xl">
-                <span>A</span><span>rticle</span><span> C</span><span>entral</span>
-            </h1>
+        <section className="flex items-center justify-between h-24 bg-gradient-to-r from-gray-700 via-gray-300 to-gray-100 px-5 shadow-lg">
+            <div className="bg-gray-800 px-4 py-2 rounded-md">
+                <h1 className=" text-4xl font-playfair font-bold text-white">
+                    <span className="text-orange-500 text-4xl px-1">N</span>
+                    <span>ews</span>
+                    <span className="text-orange-500 text-4xl px-1">C</span>
+                    <span>entral</span>
+                </h1>
+            </div>
+            
             <div className="mr-10">
                 <NavBar />
             </div>
         </section>
+        
     )
 }
 
