5bfbd73a83abe42a87dd1de985f7691f1a751600
Matt <bmatt0835@gmail.com>
2024-11-05 22:32:06 +0000

    Created User Context

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const [ user, setUser ] = useState(null);
+
+    // <<<<< CHECK IF LOGGED IN >>>>> ------
+    useEffect(() => {
+        const userInStorage = JSON.parse(localStorage.getItem("user"));
+        if (userInStorage) setUser(userInStorage);
+    }, []);
+
+    // <<<<< SAVE TO LOCAL WHEN CHANGE >>>>> ------
+    useEffect(() => {
+        if (user) {
+            localStorage.setItem("user", JSON.stringify(user));
+        } else {
+            localStorage.removeItem("user");
+        }
+    }, [user]);
+
+    const login = (username) => setUser({ username });
+    const logout = () => setUser(null);
+
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
