2277ffddd1a33ec540a019efc92f1da7df80b9e6
Matt <bmatt0835@gmail.com>
2024-11-06 20:00:00 +0000

    Initial SideBar Implementation

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+const SideBar = () => {
+  const topics = ["coding", "football", "cooking"];
+
+  return (
+    <aside className="mt-2 fixed left-0 w-24 sm:w-32 lg:w-40 bg-gray-800 text-white h-full p-2 space-y-4">
+      <h2 className="text-orange-500 text-m sm:text-lg lg:text-xl font-bold mb-4">Topics</h2>
+      <ul className="space-y-2 text-sm sm:text-lg lg:text-xl list-none">
+        {topics.map((topic) => (
+          <li key={topic}>
+            <Link
+              to={`/articles/topics/${topic}`}
+              className="block py-2 px-4 rounded hover:bg-gray-600"
+            >
+              {topic.charAt(0).toUpperCase() + topic.slice(1)}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-8">
+        <h2 className="text-orange-500 text-m sm:text-lg lg:text-xl font-bold mb-4">Navigation</h2>
+        <ul className="space-y-2 text-sm sm:text-lg lg:text-xl">
+          <li>
+            <Link to="/" className="block py-2 px-4 rounded hover:bg-gray-600">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/articles"
+              className="block py-2 px-4 rounded hover:bg-gray-600"
+            >
+              Articles
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
--- src/sections/Articles.jsx
+++ src/sections/Articles.jsx
@@ -1,11 +1,20 @@
 import ArticlesList from "../components/articles-components/ArticlesList";
+import Footer from "../components/Footer";
+import SideBar from "../components/SideBar";
 
 const Articles = () => {
   return (
-    <section className="mt-2">
+    <section className="flex flex-col min-h-screen bg-gray-800">
+      <div className="h-2 bg-orange-400 shadow-lg"></div>
+      <div className="flex flex-1">
+      <SideBar />
+      <main className="ml-24 sm:ml-32 lg:ml-40 p-8 bg-gray-50 flex-grow">
       <div>
         <ArticlesList />
       </div>
+      </main>
+      </div>
+      <div className="h-2 bg-orange-400 shadow-lg"></div>
     </section>
   );
 };
